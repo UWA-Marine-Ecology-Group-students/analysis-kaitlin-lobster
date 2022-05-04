ed5b0578a593ca710d162cc6887a8ee319ee9c62
KaitlinMcCloghry <kaitlin.mccloghry@gmail.com>
2022-05-04 15:46:44 +0800

    Finished tidying first and final choice data

--- R/structure_density_first_look.R
+++ R/structure_density_first_look.R
@@ -1,3 +1,4 @@
+install.packages("tidyverse")
 library(tidyverse)
 
 structure_density <- read.csv("data/structure_density_copy.csv")
@@ -17,11 +18,31 @@ head(struct_dens_clean)
 
 firstchoice <- data.frame("first_choice" = struct_dens_clean$first.choice,
                           "alternative" = c(struct_dens_clean$density.A + 
-                                             struct_dens_clean$density.B) - 
-                            struct_dens_clean$first.choice)
+                                            struct_dens_clean$density.B) - 
+                                            struct_dens_clean$first.choice)
+
 firstchoice$difference <- firstchoice$first_choice - firstchoice$alternative
+firstchoice$round <- struct_dens_clean$round
+firstchoice$trial <- struct_dens_clean$trial
+firstchoice$ymaze <- struct_dens_clean$ymaze
+firstchoice$lobster <- struct_dens_clean$lobster
+
+head(firstchoice)
 
 
 # then do the same for different responses: final choice
+
+finalchoice <- data.frame("final_choice" = struct_dens_clean$final.choice,
+                          "alternative" = c(struct_dens_clean$density.A + 
+                                              struct_dens_clean$density.B) - 
+                            struct_dens_clean$final.choice)
+
+finalchoice$difference <- finalchoice$final_choice - finalchoice$alternative
+finalchoice$round <- struct_dens_clean$round
+finalchoice$trial <- struct_dens_clean$trial
+finalchoice$ymaze <- struct_dens_clean$ymaze
+finalchoice$lobster <- struct_dens_clean$lobster
+
+head(finalchoice)
 
 
